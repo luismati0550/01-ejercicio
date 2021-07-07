027182053183860e5da8a82567c31a763992e5b3
Luis Angel Matias Rodriguez <201800251@colsanjose.edu.gt>
2021-07-07 00:45:07 -0600

    parte 1

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char b;
+            int l;
+            string op = "si";
+            char[] dato = new char[10];
+
+
+
+            while (op == "si")
+            {
+                Console.Clear();
+                Console.WriteLine(" \n1. Ingresar valores \n2. Mostrar valores \n3. Buscar valores");
+                l = int.Parse(Console.ReadLine());
+
+
+
+                if (l == 1)
+                {
+                    for (int i = 0; i <= 9; i++)
+                    {
+                        Console.WriteLine("i: " + i);
+                        Console.Write("Ingrese una letra:  ");
+                        dato[i] = char.Parse(Console.ReadLine());
+                    }
+                }
+                
+                Console.ReadKey();
+            }
+        }
+    }
+}
